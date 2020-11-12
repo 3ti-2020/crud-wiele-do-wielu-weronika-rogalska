d24b0dee0d225d52c50c92bf1c1c4882cf0b9901
WeronikaRo <49307070+WeronikaRo@users.noreply.github.com>
2020-11-12 17:31:07 +0100

    Add files via upload

--- index2.php
+++ index2.php
@@ -66,7 +66,7 @@ if(isset($_SESSION['zalogowany']) && $_SESSION['zalogowany'] = 1){
     <form action="wyp.php" method="POST">
         <p>Książka:</p>
         <?php
-            $result = $conn -> query("SELECT id_tytul, tytul FROM lib_tytul, lib_autor_tytul, lib_autor WHERE lib_tytul.id_tytul = lib_autor_tytul.id_tytul AND lib_autor.id_autor=lib_autor_tytul.id_autor");
+            $result = $conn -> query("SELECT id_tytul, tytul FROM lib_tytul");
             echo("<select name='tytul'>");
             while($row = $result->fetch_assoc()){
                 echo("<option name='tytul' value=".$row['id_tytul'].">".$row['tytul']."</option>");
