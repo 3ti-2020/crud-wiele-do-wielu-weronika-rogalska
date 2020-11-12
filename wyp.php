f11db99c580db8347cd18bba333ea6440c0ecbad
WeronikaRo <49307070+WeronikaRo@users.noreply.github.com>
2020-11-12 19:00:43 +0100

    Add files via upload

--- wyp.php
+++ wyp.php
@@ -1,16 +1,16 @@
 <?php
 require_once('connect.php');
 
-$login = $_POST['login'];
+$login = $_POST['login_in'];
 $tytul = $_POST['tytul'];
 $data = $_POST['date_odd'];
 
-$sql = "INSERT INTO lib_wyp VALUES (null, '$login', '$tytul', CURRENT_DATE, '$data')";
+$sql = "INSERT INTO lib_wyp VALUES (null, '$tytul', '$login', CURRENT_DATE, '$data')";
 
 echo($sql);
 
 mysqli_query($conn, $sql);
 
-header('Location: index2.php');
+// header('Location: index2.php');
 
 ?>
