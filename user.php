fa730fefb31f98ff8d2104df5985e5f31773eafb
WeronikaRo <49307070+WeronikaRo@users.noreply.github.com>
2020-11-12 19:10:34 +0100

    Update user.php

--- user.php
+++ user.php
@@ -7,6 +7,8 @@ $admin = $_POST['admin'];
 
 $sql = "INSERT INTO lib_user VALUES (null, '$login', '$password', $admin)";
 
+echo($sql);
+
 mysqli_query($conn, $sql);
 
 //header('Location: index2.php');
